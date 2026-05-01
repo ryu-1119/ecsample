a08141e021a90aa40440abd8e5f5b48a6b25a484
Ninomiya　Noriyuki <norisake404@gmail.com>
2026-05-01 18:42:37 +0900

    Modelを使って商品情報を表示、Productクラスを導入してリファクタ

--- src/main/java/com/example/ecsample/controller/HomeController.java
+++ src/main/java/com/example/ecsample/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.controller;
+package com.example.ecsample.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ src/main/java/com/example/ecsample/controller/ProductController.java
@@ -0,0 +1,58 @@
+package com.example.ecsample.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.ecsample.entity.Product;
+
+@Controller
+public class ProductController {
+
+	ProductController(HomeController homeController) {
+	}
+
+	@GetMapping("/products")
+	public String showList(Model model) {
+		//商品一覧ページを表示する
+		List<Product> products = Arrays.asList(
+				new Product(1, "コーヒーカップ", 1200),
+				new Product(2, "ティーポット", 2400),
+				new Product(3, "マグカップ", 1500));
+		model.addAttribute("products", products);
+		return "product/list";
+	}
+
+	@GetMapping("/product/{id}")
+	public String showDetail(@PathVariable("id") int id, Model model) {
+		Product product;
+		if (id == 1) {
+			product = new Product(1, "コーヒーカップ", 1200);
+		} else if (id == 2) {
+			product = new Product(2, "ティーポット", 2400);
+		} else {
+			product = new Product(0, "未登録の商品", 0);
+		}
+		model.addAttribute("product", product);
+		return "product/detail";
+	}
+
+	//	@GetMapping("/product/{id}")
+	//	public String productDetail(
+	//			@PathVariable("id") int id,
+	//			@RequestParam(value = "coupon", required = false) String coupon
+	//
+	//	) {
+	//		System.out.println("アクセスされたＩＤ：" + id);
+	//		if (coupon != null) {
+	//			System.out.println("クーポン:" + coupon);
+	//		} else {
+	//			System.out.println("クーポンなし");
+	//		}
+	//		return "product/detail";
+	//	}
+}
